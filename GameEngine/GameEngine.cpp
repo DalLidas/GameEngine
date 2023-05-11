1949a655aaa7a70d743f7836e7cee55f60f80760
DalLidas <126471477+DalLidas@users.noreply.github.com>
2023-05-11 22:55:20 +0300

    Добавьте файлы проекта.

---
+++ GameEngine/GameEngine.cpp
@@ -0,0 +1,30 @@
+﻿// GameEngine.cpp : Определяет точку входа для приложения.
+//
+//#include "GameEngine.h"
+
+#include <windows.h>
+
+#include "WinClass.h"
+
+int CALLBACK wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nShowCmd){
+
+	WinClass windows(400, 400);
+
+	MSG msg = { 0 };
+	while (true) {
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT) {
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
---
+++ GameEngine/WinClass.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <windows.h>
+
+#ifdef _UNICODE
+# define _T(str) L##str
+#else
+# define _T(str) str
+#endif
+
+class WinClass {
+public:
+	WinClass(int weight, int height);
+};
